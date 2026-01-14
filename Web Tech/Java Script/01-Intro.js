4e0c1b3b4c9e429d699f6a4806f567797fc24d86
guruasTrainer2024 <guruastrainer2024@gmail.com>
2026-01-14 08:19:18 +0530

    HTML CSS JS commit

---
+++ Web Tech/Java Script/01-Intro.js
@@ -0,0 +1,11 @@
+var a;
+var b;
+a = prompt("Enter Number 1")
+b = prompt("Enter Number 2");
+var c, d;
+c = parseInt(a)
+d = parseInt(b)
+var e = c + d;
+// document.write(e)
+//alert(e);
+console.log(e)
---
+++ Web Tech/Java Script/01-welcome.js
@@ -0,0 +1,1 @@
+alert("Wish you good luck")
